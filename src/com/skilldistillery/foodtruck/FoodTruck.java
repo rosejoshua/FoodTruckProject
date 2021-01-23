c7057ae98b5793778e1fc3da9f186c7361203904
Joshua Rose <jlr007@protonmail.com>
2021-01-23 13:15:03 -0500

    added fields and constructor to FoodTruck

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,5 +1,20 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
+	
+	private static int nextId=0;
+	private int id;
+	private String name;
+	private String foodType;
+	private double rating;
+	
+	public FoodTruck(String name, String foodType, double rating) {
+		this.id = nextId;
+		nextId++;
+		
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+	}
 
 }
